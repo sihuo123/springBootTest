d09c73521aef54d1b3d7d433a38e1c0a98582d58
sihuo123 <295705647@qq.com>
2019-06-21 10:33:40 +0800

    redis 整合

--- src/test/java/com/springboot/demo/SpringbootApplicationTests.java
+++ src/test/java/com/springboot/demo/SpringbootApplicationTests.java
@@ -3,6 +3,8 @@ package com.springboot.demo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -11,6 +13,27 @@ public class SpringbootApplicationTests {
 
     @Test
     public void contextLoads() {
+    }
+
+    @Autowired
+    RedisUtil redisUtil;
+
+    @Test
+    public void testRedis(){
+        String id = "0753c996-4a52-45fd-af80-4e4a954542b2";
+        Boolean hasKey = redisUtil.hasKey(id);
+
+        if(!hasKey){
+            System.out.println("从数据库读取");
+            GoodsInfoJy g = goodsInfoJyMapper.selectByPrimaryKey(id);
+            redisUtil.set(id, g);
+            System.out.println(g);
+        } else {
+            System.out.println("从缓存读取");
+            GoodsInfoJy g = (GoodsInfoJy)redisUtil.get(id);
+            System.out.println(g);
+        }
+
     }
 
 }
